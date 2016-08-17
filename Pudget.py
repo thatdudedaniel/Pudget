65b9810a33fd023dd98382631a30381175dd003a
daniel <daniellewis1050@yahoo.com>
2016-08-16 22:56:38 -0500

    Updateded Readme

---
+++ Pudget.py
@@ -0,0 +1,20 @@
+#version 1.0
+
+#Fill in list of bills here
+Bills=[650,30,60,20,65,97]
+
+def add(Bills):
+    Total=0
+    for i in Bills:
+        Total+=i
+        return Total
+
+#Your Paycheck goes here
+paycheck=900
+
+result=add(Bills)
+
+print("Pacheck:", paycheck)
+print("Total amount of bills per month:", result)
+print("Total amount needed to save each check:", result /2)
+print("Leftover each check:", result /2 - paycheck)
---
+++ README.md
@@ -0,0 +1,2 @@
+# Pudget
+Simple budget calculator that tells you how much you need to save each check to cover your bills and how much you have leftover, just fill in the values
